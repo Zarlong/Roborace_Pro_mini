bd0c13055c8e5829694c9bd3a0b363510a4e178e
Николай Коковкин <79041818+Zarlong@users.noreply.github.com>
2023-10-28 19:45:54 +0300

    Update ROBORACE.ino

--- ROBORACE/ROBORACE.ino
+++ ROBORACE/ROBORACE.ino
@@ -8,16 +8,12 @@ int pingPin2 = IN2;
 int pingPin3 = IN3;
 int IK = IN4;
 
-//int MAS[9];
-//int MASS[9];
 int C1 = 0;
 int C2 = 0;
 int C3 = 0;
 int kservo = 120;
 const int motor_number1 = 1;
 const int motor_number2 = 2;
-//long duration1, inches1, cm1;
-//long duration2, inches2, cm2;
 
 float Kp = 0.27;
 float Ki = 0.02;
@@ -36,16 +32,14 @@ const int NUM_READ = 5;
 unsigned long long pidt = 0;
 int pidold = 0;
 
-byte idx1 = 0;                // индекс
-float valArray1[NUM_READ];    // массив
+// byte idx1 = 0;                // индекс
+// float valArray1[NUM_READ];    // массив
+// byte idx2 = 0;                // индекс
+// float valArray2[NUM_READ];    // массив
+// byte idx3 = 0;                // индекс
+// float valArray3[NUM_READ];    // массив
 
-byte idx2 = 0;                // индекс
-float valArray2[NUM_READ];    // массив
-
-byte idx3 = 0;                // индекс
-float valArray3[NUM_READ];    // массив
-
-float EGOR = 0;
+// float EGOR = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -67,22 +61,22 @@ void loop() {
 //  C2 = constrain(FILT2(cm(pingPin2)), 6, 100);
 //  C3 = FILT3(cm(pingPin3));
   //  forward(90);
-  Serial.print(C1);
-  Serial.print('\t');
-  Serial.print(C2);
-  Serial.print('\t');
-  Serial.println(C3);
-//  backward(70);
-//  backward_err();
-//  //  pid();
-//  if (C1 > 35 and C2 > 35) {
-//    servo.write(kservo);
+  // Serial.print(C1);
+  // Serial.print('\t');
+  // Serial.print(C2);
+  // Serial.print('\t');
+  // Serial.println(C3);
+ backward(70);
+ backward_err();
+ //  pid();
+ if (C1 > 35 and C2 > 35) {
+   servo.write(kservo);
+   forward(90);
+ }
+ else {
+   pid();
 //    forward(90);
-//  }
-//  else {
-//    pid();
-////    forward(90);
-//  }
+ }
 }
 int cm(int a) {
   pinMode(a, OUTPUT);
@@ -98,15 +92,7 @@ int cm(int a) {
 }
 
 void pid() {
-  //  if (C1 > 40 and C2 > 40) {
   E = C1 - C2;
-
-  //  Serial.print(C2);
-  //  Serial.print(" ");
-  //  Serial.print(C1);
-  //  Serial.print(" ");
-  //  Serial.println(C3);
-
   P = E;
   I = (I * 0.95) + E;
   D = Eold - E;
@@ -116,9 +102,7 @@ void pid() {
   int A = kservo + PID;
   A = constrain(A, 90, 150);
   servo.write(A);
-
-  EGOR = PID * 0.5;
-
+  // EGOR = PID * 0.5;
 }
 
 void backward_err() {
@@ -176,7 +160,7 @@ void forward(int s) {
   int T = constrain(C3, 0, 80);
   T += (constrain(C1, 0, 80) + constrain(C2, 0, 80)) * 0.8;
   T /= 2;
-  T = map(T, 20, 80, 0, s);
+  T = map(T, 20, 80, -20, s); // Поменял на -20 надо оттестить 
   Trackduino.motor(motor_number1, T);
   Trackduino.motor(motor_number2, T);
   //  Serial.print(T+EGOR);
@@ -184,32 +168,32 @@ void forward(int s) {
   //  Serial.println(T-EGOR);
 }
 
-float FILT1(float newVal) {
-  valArray1[idx1] = newVal;
-  if (++idx1 >= NUM_READ) idx1 = 0;
-  float average = 0;
-  for (int i = 0; i < NUM_READ; i++) {
-    average += valArray1[i];
-  }
-  return (float)average / NUM_READ;
-}
+// float FILT1(float newVal) {
+//   valArray1[idx1] = newVal;
+//   if (++idx1 >= NUM_READ) idx1 = 0;
+//   float average = 0;
+//   for (int i = 0; i < NUM_READ; i++) {
+//     average += valArray1[i];
+//   }
+//   return (float)average / NUM_READ;
+// }
 
-float FILT2(float newVal) {
-  valArray2[idx2] = newVal;
-  if (++idx2 >= NUM_READ) idx2 = 0;
-  float average = 0;
-  for (int i = 0; i < NUM_READ; i++) {
-    average += valArray2[i];
-  }
-  return (float)average / NUM_READ;
-}
+// float FILT2(float newVal) {
+//   valArray2[idx2] = newVal;
+//   if (++idx2 >= NUM_READ) idx2 = 0;
+//   float average = 0;
+//   for (int i = 0; i < NUM_READ; i++) {
+//     average += valArray2[i];
+//   }
+//   return (float)average / NUM_READ;
+// }
 
-float FILT3(float newVal) {
-  valArray3[idx3] = newVal;
-  if (++idx3 >= NUM_READ) idx3 = 0;
-  float average = 0;
-  for (int i = 0; i < NUM_READ; i++) {
-    average += valArray3[i];
-  }
-  return (float)average / NUM_READ;
-}
+// float FILT3(float newVal) {
+//   valArray3[idx3] = newVal;
+//   if (++idx3 >= NUM_READ) idx3 = 0;
+//   float average = 0;
+//   for (int i = 0; i < NUM_READ; i++) {
+//     average += valArray3[i];
+//   }
+//   return (float)average / NUM_READ;
+// }
