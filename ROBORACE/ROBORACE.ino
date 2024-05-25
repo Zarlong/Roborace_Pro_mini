a08eb40bf58c8b3d62ac890993bfaed41656a52b
Nikolay Kokovkin <79041818+Zarlong@users.noreply.github.com>
2024-05-25 12:29:15 +0300

    Update ROBORACE.ino

--- ROBORACE/ROBORACE.ino
+++ ROBORACE/ROBORACE.ino
@@ -54,65 +54,17 @@ void setup() {
   Serial.begin(9600);
   Trackduino.setup();
   servo.attach(OUT1);
-  // for (int i = 0; i < NUM_READ; ++i) {
-  //   C1 = constrain(cm(pingPin1), 6, 250);
-  //   C2 = constrain(cm(pingPin2), 6, 250);
   C3 = constrain(cm(), 6, 250);
-  // }
-  // servo.write(110);
-  // delay(1000);
-  // servo.write(145);
-  // delay(1000);
   //  pinMode(IK, INPUT);
 }
 void loop() {
-  // delay(1000)
-  // C1 = FILT1(constrain(cm(pingPin1), 2, 200));
-  // C2 = FILT2(constrain(cm(pingPin2), 2, 200));
-  // // C1 = cm(pingPin1);
-  // // C2 = cm(pingPin2);
-  // C3 = FILT3(constrain(cm(pingPin3), 2, 100));
-
-  //  forward(90);
-  // Serial.print(C1);
-  // Serial.print('\t');
-  // Serial.print(C3);
-  // Serial.print('\t');
-  // Serial.println(C2);
-  //  backward(70);
-  //  backward_err();
-  //  //  pid();
-  //  if (C1 > 30 and C2 > 30) {
-  //    servo.write(kservo);
-  //    forward(90);
-  //  }
-  //  else {
-  //    pid();
-  // if (C1 > L and C2 > L) {
-  //   servo.write(kservo);
-  //   forward(80);
-  // } else {
   C3 = constrain(cm(), 6, 50);
   pid();
   forward(98);
   // }
   // forward(80);
-  backward(60);
+  backward(70);
   // backward_err();
-
-  // C1 = abs(constrain(cm(pingPin1), 6, 250));
-  // C2 = abs(constrain(cm(pingPin2), 6, 250));
-  // C3 = abs(constrain(cm(pingPin3), 6, 250));
-  // Serial.print(C1);
-  // Serial.print('\t');
-  // Serial.print(C2);
-  // Serial.print('\t');
-  // Serial.print(A);
-  // Serial.print('\t');
-  // Serial.print(findMedianN1(sha1()));
-  // Serial.print('\t');
-  // Serial.println(findMedianN2(sha2()));
-  //  }
 }
 int cm() {
   pinMode(IN3, OUTPUT);
@@ -153,27 +105,6 @@ void pid() {
   // C3 = abs(constrain(cm(pingPin3), 6, 250));
 
   E = sha1() - sha2() + 30;
-  // if (C1 > L and C2 < L) {
-  //   F = 1;
-  //   E = -50; // СЕ -30 C2 -L
-  //   Trackduino.RGB_off();
-  //   Trackduino.RGB_red();
-  // } else if (C2 > L and C1 < L) {
-  //   F = 2;
-  //   E = 50; //СЕ 30 L - C1
-  //   Trackduino.RGB_off();
-  //   Trackduino.RGB_green();
-  // } else if (C2 > L and C1 > L) {
-  //   F = 3;
-  //   E = 0;
-  // } else if (C1 < L and C2 < L){
-  //   F = 4;
-  //   E = C1 - C2;
-  //   Trackduino.RGB_off();
-  //   Trackduino.RGB_blue();
-  // }
-
-  // E = C2 - C1
   P = E;
   // I = (I * 0.93) + E;
   D = Eold - E;
@@ -230,16 +161,6 @@ void backward(int s) {
       Trackduino.motor(motor_number1, -s);
       Trackduino.motor(motor_number2, -s);
     }
-    //   servo.write(95);
-    //   Trackduino.motor(motor_number1, s);
-    //   Trackduino.motor(motor_number2, s);
-    //   delay(400);
-    // } else {
-    //   servo.write(145);
-    //   Trackduino.motor(motor_number1, s);
-    //   Trackduino.motor(motor_number2, s);
-    //   delay(400);
-    // }
   }
 }
 void forward(int s) {
@@ -268,8 +189,8 @@ void forward(int s) {
   // T += (abs(constrain(findMedianN1(cm(pingPin1)), 4, 200)) + abs(constrain(findMedianN2(cm(pingPin2)), 4, 200))) * 0.5;
   // T /= 2;
   // T = map(T, 20, 80, -30, s);  // Поменял на -20 надо оттестить
-  Trackduino.motor(motor_number1, s);
-  Trackduino.motor(motor_number2, s);
+  Trackduino.motor(motor_number1, s - (E * 0.2)); // Требует тестов
+  Trackduino.motor(motor_number2, s + (E * 0.2));
   // Serial.println(s - T);
   //  Serial.print(T+EGOR);
   //  Serial.print('\t');
